0c36a765dbf3436f8f175ac0126a2d11e3a9cf6b
admin user <adminuser@D-10-65-82-118.dhcp4.washington.edu>
2018-11-26 17:32:46 -0800

    class

---
+++ codeFolder/divideAndCon.py
@@ -0,0 +1,19 @@
+in1 = open("char.cnt", 'r')
+#out1 = open("ch.csv", 'w')
+cc = [0] * 26
+for line in in1:
+    line.lower()
+    for ch in line:
+        if ord(ch) >= ord("a") and ord(ch) <= ord("z"):
+            cc[ord(ch) - ord("a")] += 1
+print(cc)
+letter = 0
+while letter < len(cc):
+    print("{0:a},{1:d}\n".format(chr(ord("a")) + letter, cc[letter]))
+    letter += 1
+in1.close()
+#out1.close()
+
+
+
+
---
+++ codeFolder/histoProgram.py
@@ -0,0 +1,31 @@
+def main():
+    # open up input
+    infile = open("scores.txt", 'r')
+    # create counters list
+    stuff = create_lists(infile)
+    # display histogram
+    display_Histo(stuff)
+    # display history
+def create_lists(a):
+    ses = []
+    scoreCuts = [0] * 101
+    for line in a:
+        score = int(line)
+        scoreCuts[score] = scoreCuts[score] + 1
+    i = 0
+    while i < 101:
+        i += 1
+        ses.append(0)
+    return scoreCuts
+def display_Histo(a):
+    index = 0
+    counter = 0
+    otherCounter = 0
+    while index < len(a):
+        print("{0}:".format(index), end = '')
+        index += 1
+        stats = 0
+    return counter
+        
+
+main()
