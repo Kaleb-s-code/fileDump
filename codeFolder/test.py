cfabd9f1983d8c77124cdf329aa3b356c200a74c
admin user <adminuser@admins-MacBook-Pro.local>
2018-11-28 12:44:29 -0800

    making changes

--- codeFolder/test.py
+++
@@ -1,7 +0,0 @@
-print("Hello there")
-# in1 = open("scores.txt", 'r')
-# for line in in1
-print("Hello")
-print("It took me the entire night to get my path situatuated..FML")
-print("Now, I shouldn't have anymore path problems, and all my extensions should work")
-print("Done and done son.")
