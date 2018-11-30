42d139da87395dbbac4b13cb9902455161a1df54
TheBoss <kaleb.s.mor@gmail.com>
2018-11-30 10:37:54 -0800

    y

---
+++ codeFolder/scratchPaperProjectTwo.py
@@ -0,0 +1,20 @@
+
+
+    # This is supposed to get the totals of healthy people into a list
+    # while idx < 12:
+    #     for line in a:
+    #         var = line.split(",")
+    #         if line[-2] == '0':
+    #             container.append(var[idx])
+    #             # This checks for questionmarks and replaces them with str 0
+    #             if var[idx] == '?':
+    #                 var[idx] = var[idx].replace("?", '0')
+    #                 container.append(var[idx])
+    #                 container.remove("?")
+    #                 totalsContainer[idx] += float(var[idx])
+    #                 ifCount += 1
+    #             else:
+    #                 totalsContainer[idx] += float(var[idx])
+    #                 elseCount += 1
+    #         if idx < 12:
+    #             idx += 1
