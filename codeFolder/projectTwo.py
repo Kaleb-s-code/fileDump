43b02ca02e3b45610be2e683648f6f4ec5a8e91a
TheBoss <kaleb.s.mor@gmail.com>
2018-12-03 01:01:39 -0800

    finally

--- codeFolder/projectTwo.py
+++ codeFolder/projectTwo.py
@@ -2,14 +2,14 @@ def main():
     # getFiles()
     in1 = open("codeFolder/train.csv", 'r')
     healthyIll = countersAccumulators(in1)
-    # in1.close()
-    # in1 = open("codeFolder/train.csv", 'r')
+    in1.close()
+    in1 = open("codeFolder/train.csv", 'r')
     patientTotal = totalPatients(in1)
-    # in1.close()
-    # in1 = open("codeFolder/train.csv", 'r')
+    in1.close()
+    in1 = open("codeFolder/train.csv", 'r')
     healthyAvs = getHealthyAverage(in1)
-    # in1.close()
-    # in1 = open("codeFolder/train.csv", 'r')
+    in1.close()
+    in1 = open("codeFolder/train.csv", 'r')
     unHealthyAvs = getUnhealthyAverages(in1)
     in1.close()
     classSeps = getClassSeps(healthyAvs, unHealthyAvs)
@@ -18,10 +18,6 @@ def main():
     newData = compareData(in1, classSeps)
     in1.close()
     getProperPresentation(patientTotal, healthyIll[0], healthyIll[1],healthyAvs, unHealthyAvs, classSeps,newData)
-# def getFiles():
-#     in1 = open("codeFolder/train.csv", 'r')
-#     in2 = open("csvFiles/cleveland.csv", 'r')
-    # return in1, in2
 def countersAccumulators(a):
     # This function can return the total of healthy people and ill people (in that order), and the total numneber of files processed. 
     statusContainer = []
@@ -101,11 +97,9 @@ def compareData(a, b):
     test = []
     test2 = []
     count = 0
-    countClev = 0
     combine = []
     value = 0
     difFile = False
-    # This is just splitting up the line and adding index zero to the test list
     for line in a:
         var = line.split(",")
         var[-1] = var[-1].strip("\n")
@@ -116,23 +110,12 @@ def compareData(a, b):
         if len(var) > len(b):
             var = var[1:]
         for idx in range(13):
-            # print("at Risk moved up:", atRisk, "Places")
-            # print("\n")
-            # print("Before check: ", var[idx], "____",b[idx])
             if var[idx] == "?":
-                # print(var[idx])
                 var[idx] = var[idx].replace("?", "0") 
             if float(var[idx]) > b[idx]:
-                # print("\n")
-                # print("During Check: ", var[idx], "____",b[idx])
-                # print("at Risk AFTER check:", atRisk)
                 atRisk += 1
-        #Now we have left the for loop in range 13
-        #This is checking to see what file we are dealing with 
         if var[-1] > "0" and difFile == False:
             actuallySick += 1
-
-        # This is where we determine if the person is sick or not, and add it to the list that will be sent to a csv file. This is where things are going wrong I think
         if atRisk >= 7:
             count += 1
             atRisk = 0
@@ -142,7 +125,6 @@ def compareData(a, b):
             test2.append("0")
         if atRisk < 7:
             atRisk = 0
-    # This while loop is getting things ready for the csv
     while value < len(test):
         combine.append(test[value])
         combine.append(",")
@@ -151,9 +133,12 @@ def compareData(a, b):
         value += 1
     for item in combine:
         out1.write(item)
-    #13 too many sick counted
     somevar = (actuallySick / sickPeople) * 100
     print("-" * 50)
+    if difFile == True:
+        print("Reading from Clevlend.csv")
+    else:
+        print("Reading from train.csv")
     print("sick prediction:", sickPeople)
     print("actually sick:", actuallySick)
     print("prediction rate: %{0:.2f}".format(somevar))
