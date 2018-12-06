2a4750a3b1b0c562140991f18561173c3ec16ff0
admin user <adminuser@D-10-65-82-118.dhcp4.washington.edu>
2018-12-05 17:37:55 -0800

    more

--- codeFolder/Grades.java
+++ codeFolder/Grades.java
@@ -17,6 +17,16 @@ public class Grades {
         int[] actualGrades = getScores(input, gradesCount);
         System.out.println(convertArrayToString(gradesCount));
         System.out.println(convertArrayToString(actualGrades));
+        displayHistogram(gradesCount);
+        double average = getAverage(actualGrades);
+        System.out.println("Class average = " + average + "%");
+    }
+    public staticdouble getAverage(int[] theA) {
+        double total = 0.0;
+        for (int i = 0; i < theA.length; i++) {
+            total += theA[i];
+        }
+        return total / theA.length;
     }
     public static int[] getScores(Scanner theIn, int[] theGradesCnt) {
         int[] grades = new int[theIn.nextInt()];
@@ -28,6 +38,19 @@ public class Grades {
             index ++;
         }
         return grades;
+    }
+    public static void displayHistogram(int[] theA) {
+        int index = 0;
+        while (index < theA.length) {
+            if (theA[index] > 0) {
+                System.out.print(String.format("%3d: ", index));
+                for (int starCount = 0; startCount < theA[index]; starCount ++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        index ++;
+        }
     }
     public static String convertArrayToString(int[] theA) {
         String result = "[" + theA[0];
