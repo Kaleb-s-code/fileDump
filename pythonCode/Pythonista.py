84f9398d051d1541213e03dd783dd085e23e88fe
encrypt-admin <encrypt-admin@users.noreply.github.com>
2018-12-12 12:25:38 -0800

    Update Pythonista.py

--- pythonCode/Pythonista.py
+++ pythonCode/Pythonista.py
@@ -34,18 +34,25 @@ import codecs
 def main():
     infile = codecs.open("transactions.csv", "r", "utf-8")
     initial = getInitialResponse()
-    handler = processUserInput(initial, infile)
-    
-    infile.close()
+    handler = processUserInput(initial)
+    if handler == 1:
+    	getDatesAndUserInput(infile)
+    	infile.close()
+    elif handler == 2:
+    	findBills(infile)
+    	infile.close()
+    elif handler == 3:
+    	getCashFlow(infile)
+    	infile.close()
 def getInitialResponse():
    resList = ["1", "2", "3"]
    response1 = input("Please select one of the predefined options:\n\n1. Show Transactions\n2. Find Bills\n3. Cash Flow\n\nEnter 1,2 or 3: ")
    while response1 not in resList:
-    response1 = input("Enter 1, 2 or 3: ")
+   	response1 = input("Enter 1, 2 or 3: ")
    print("\n")
    return response1
 # This needs user input 
-def processUserInput(res1, in1):
+def processUserInput(res1):
     res1 = int(res1)
     yesList = ["Yes", "y", "Y", "YES"]
     noList = ["n", "No", "no","NO","nO", "Nope"]
@@ -56,57 +63,63 @@ def processUserInput(res1, in1):
 	    	while var not in yesList and res1 == 1:
 	    		var = input("You have selected Show Transactions. Continue?(y/n): ")
 	    		if var in noList:
-	    			res1 = int(getInitialResponse())
 	    			done = False
+	    			res1 = int(input("Enter 1,2 or 3: "))
+	    			print("\n")
 	    		if var in yesList:
 	    			done = True
 	    			if done == True:
-	    				getDatesAndUserInput(in1)
+	    				return 1
 	    elif res1 == 2:
 	    	while var not in yesList and res1 == 2:
 	    		var = input("You have selected Find Bills. Continue?(y/n): ")
 	    		if var in noList:
-	    			res1 = int(getInitialResponse()) 
 	    			done = False
+	    			res1 = int(input("Enter 1,2 or 3: "))
+	    			print("\n")
 	    		if var in yesList:
 	    			done = True
 	    			if done == True:
-	    				findBills(in1)
+	    				return 2
 	    elif res1 == 3:
 	    	while var not in yesList and res1 == 3:
 	    		var = input("You have selected Cash Flow. Continue?(y/n): ")
-	    		if var in noList:
-	    			res1 = int(getInitialResponse()) 
+	    		if var in noList: 
 	    			done = False
+	    			res1 = int(input("Enter 1,2 or 3:"))
+	    			print("\n")
 	    		if var in yesList:
 	    			done = True
 	    			if done == True:
-	    				getCashFlow(in1)
-	mainMenu = input("return to main menu?: ")
-	if mainMenu == "y":
-		res1 = int(getInitialResponse())
+	    				return 3
 def getDatesAndUserInput(in1):
     print("\nYou are viewing transactions from:\n{}\n".format(in1.name))
     in1.readline()
     idx = 0
+    count = 0
     dateContainer = []
     # This containes max date and min date
     retContainer = []
     for line in in1:
         var = line.split(",")
-        var[-1] = var[-1].strip("\n").strip("$")
+        var[-1] = var[-1].strip("\n").strip("$").strip('""')
+        if var[-1] < "0":
+        	var[-1] = "0.00"
+        count += float(var[-1])
         dateContainer.append(var[0])
     retContainer.append(max(dateContainer))
     retContainer.append(min(dateContainer))
     print("Most Recent Transaction Date: {}\nOldest Transaction Date: {}".format(retContainer[1], retContainer[0]))
-    response2 = input("\nSelect An Option From Below:\n1. Show total spent to date\n2. Show spending with given date range\n3. Show totals per category\n4. Show category spending in a range\n5. Show averages")
-    return response2
+    response2 = int(input("\nSelect An Option From Below:\n1. Show total spent to date\n2. Show spending with given date range\n3. Show totals per category\n4. Show category spending in a range\n5. Show averages"))
+    if response2 == 1:
+    	print("\n")
+    	print("Total money moved: ${0:,.2f}".format(count))
 def findBills(in1):
 	for line in in1:
 		print(line)
 def getCashFlow(in1):
 	for line in in1:
 		var = line.split(",")
-		var[-1] = var[-1].strip("\n")
+		var[-1] = var[-1].strip("\n").strip("$")
 		print(var[-1])
 main()
