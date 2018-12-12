b7b045a3c249b0605cbeb818687dda5d303aa9d6
TheBoss <kaleb.s.mor@gmail.com>
2018-12-11 21:47:05 -0800

    yay

--- JavaCode/newProject/src/app/App.java
+++ JavaCode/newProject/src/app/App.java
@@ -1,7 +1,13 @@
-package app;
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+    public static void main(String[] args) {
+        
+        System.out.println("Hello... You filthy animal");
+        
+        int myNum = 10 * 5;
+        int anotherNum = 43 * 7;
+        int newNum = anotherNum * 3;
+        System.out.println(myNum);
+        System.out.println(anotherNum);
+        System.out.println(newNum);
     }
 }
