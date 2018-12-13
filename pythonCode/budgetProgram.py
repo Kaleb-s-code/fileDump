c22f4e667c7c87823f1c18320972c49e89ea5413
TheBoss <kaleb.s.mor@gmail.com>
2018-12-13 02:05:45 -0800

    working on that total

--- pythonCode/budgetProgram.py
+++ pythonCode/budgetProgram.py
@@ -27,7 +27,6 @@
    # per week, per month 
    # project total needed income to be able to 
    # save a user defined amount per month 
-import datetime
 def main():
     infile = open("C:/Users/Kaleb/Documents/fileDump/csvFiles/transactions.csv")
     initial = getInitialResponse()
@@ -43,22 +42,22 @@ def main():
     	infile.close()
 def getInitialResponse():
    resList = ["1", "2", "3"]
+   # Needs input validation here
    response1 = input("Please select one of the predefined options:\n\n1. Show Transactions\n2. Find Bills\n3. Cash Flow\n\nEnter 1,2 or 3: ")
    while response1 not in resList:
    	response1 = input("Enter 1, 2 or 3: ")
    print("\n")
    return response1
-# This needs user input 
 def mainMenuValidation(res1):
     res1 = int(res1)
-    yesList = ["Yes", "y", "Y", "YES"]
+    yesList = ["yes","Yes", "y", "Y", "YES"]
     noList = ["n", "No", "no","NO","nO", "Nope"]
     var = 'null'
     done = True
     while res1 > 0:
 	    if res1 == 1:
 	    	while var not in yesList and res1 == 1:
-	    		var = input("You have selected Show Transactions. Continue?(y/n): ")
+	    		var = input("You have selected Show Transactions. Continue?: ")
 	    		if var in noList:
 	    			done = False
 	    			res1 = int(input("Enter 1,2 or 3: "))
@@ -69,7 +68,7 @@ def mainMenuValidation(res1):
 	    				return 1
 	    elif res1 == 2:
 	    	while var not in yesList and res1 == 2:
-	    		var = input("You have selected Find Bills. Continue?(y/n): ")
+	    		var = input("You have selected Find Bills. Continue?: ")
 	    		if var in noList:
 	    			done = False
 	    			res1 = int(input("Enter 1,2 or 3: "))
@@ -80,7 +79,7 @@ def mainMenuValidation(res1):
 	    				return 2
 	    elif res1 == 3:
 	    	while var not in yesList and res1 == 3:
-	    		var = input("You have selected Cash Flow. Continue?(y/n): ")
+	    		var = input("You have selected Cash Flow. Continue?: ")
 	    		if var in noList: 
 	    			done = False
 	    			res1 = int(input("Enter 1,2 or 3:"))
@@ -90,34 +89,46 @@ def mainMenuValidation(res1):
 	    			if done == True:
 	    				return 3
 def transactionData(in1):
+    print("=" * 25)
     print("\nYou are viewing transactions from:\n{}\n".format(in1.name))
     in1.readline()
-    idx = 0
     count = 0
+    idx = 0
+    test = [0] * 25
     response2 = 0
     dateContainer = []
+    expenses = []
+    amounts = []
+    expenseList = ["Mortgage", "HOA", "Student Loans", "iPad", "Car-Insurance", "Sprint", "Xfinity", "Medical & Dental", "Groceries",  "Donations", "Allowance", "Netflix", "HP Instant Ink", "YMCA", "Fuel", "PSE", "Home Needs", "State Farm", "Healthy Paws", "Date Night", "Wishlist", "Other", "Buffer", "Imperfect Produce", "Ark Server"]
     nonExpenseList = ["Starting Balance:", "Additions:", "Digit", "Rebalancing:", "Expected Savings", "S balance +:", "S balance -:"]
-    yesList = ["Yes", "y", "Y", "YES"]
+    yesList = ["yes", "Yes", "y", "Y", "YES"]
     date1 = ""
     date2 = ""
     maximumMinimumDates = []
+# This loop needs to increase a value in test based on the index and prescense
     for line in in1:
         var = line.split(",")
         var[-1] = var[-1].strip("\n").strip("$").strip('""')
         if var[-1] < "0":
-        	var[-1] = "0.00"
+            var[-1] = "0.00"
         if var[3] not in nonExpenseList:
+            expenses.append(var[3])
+            amounts.append(float(var[-1]))
             count += float(var[-1])
         dateContainer.append(var[0])
     maximumMinimumDates.append(max(dateContainer))
     maximumMinimumDates.append(min(dateContainer))
+    maxAmountIndex = amounts.index(max(amounts))
+    minAmountIndex = amounts.index(min(amounts))
+    print("The largest expense was: ${}\nFrom the category: '{}'".format(amounts[maxAmountIndex], expenses[maxAmountIndex]))
+    print("\nThe Smallest expense was: ${}\nFrom the category '{}'".format(amounts[minAmountIndex], expenses[minAmountIndex]))
     while response2 == 0:    
-        print("Most Recent Transaction Date: {}\nOldest Transaction Date: {}".format(maximumMinimumDates[0], maximumMinimumDates[1]))
+        print("\nMost Recent Transaction Date: {}\nOldest Transaction Date: {}".format(maximumMinimumDates[0], maximumMinimumDates[1]))
         response2 = int(input("\nSelect An Option From Below:\n1. Show total spent to date\n2. Show spending with given date range\n3. Show totals per category\n4. Show category spending in a range\n5. Show averages\nEnter a number: "))
         if response2 == 1:
             print("\n")
             print("Total spent up to {0}: ${1:,.2f}".format(maximumMinimumDates[0], count))
-            repeat = input("Back to menu?:" )
+            repeat = input("Back to menu?: ")
             print("\n")
             if repeat in yesList:
                 response2 = 0
@@ -135,10 +146,12 @@ def transactionData(in1):
                 if date2 < date1:
                     date2 = ""
             dateRanges(date1, date2)
-            repeat = input("Back to menu?:" )
+            repeat = input("Back to menu?: ")
             print("\n")
             if repeat in yesList:
                 response2 = 0
+        elif response2 == 3:
+            print(test)
 def dateRanges(d1, d2):
     in1 = open("C:/Users/Kaleb/Documents/fileDump/csvFiles/transactions.csv")
     in1.readline()
