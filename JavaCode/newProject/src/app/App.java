bca8aa6cd7e3b07a59c7ea4b48d42ebe15ac8f2a
TheBoss <kaleb.s.mor@gmail.com>
2018-12-15 23:26:29 -0800

    1234

--- JavaCode/newProject/src/app/App.java
+++ JavaCode/newProject/src/app/App.java
@@ -9,5 +9,29 @@ public class App {
         System.out.println(myNum);
         System.out.println(anotherNum);
         System.out.println(newNum);
+        char symbol = '\u00AE';
+        System.out.println(symbol);
+        boolean bob = false;
+        if (bob == false)
+            System.out.println("Titty smack");
+        int topScore = 5000;
+        if (topScore == 5000)
+            System.out.println("Oh snap, you gonna git pussy");
+        if (bob == false && topScore > 4000)
+            System.out.println("And you gonna lick it too");
+        if ((bob == false) || topScore < 50)
+            System.out.println("And you might even get to put it in the stinky hole");
+        boolean isCar = false;
+        boolean wasCar = isCar ? true: false;
+        System.out.println("This is a string: " + wasCar);
+        // Chanllange of sorts 
+        double aNum = 20d;
+        double stupidNum = 80d;
+        double someNum = (aNum + stupidNum) * 25d;
+        System.out.println(someNum);
+        double remainder = someNum % 40d;
+        System.out.println("THis is the remainder: " + remainder);
+        if (remainder <= 20)
+            System.out.println("Total was over the limit!");
     }
 }
