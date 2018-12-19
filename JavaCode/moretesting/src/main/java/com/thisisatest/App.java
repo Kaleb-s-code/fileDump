83eceea0abaa1107bd7c24b3cb3beed25a67004e
TheBoss <kaleb.s.mor@gmail.com>
2018-12-19 00:26:54 -0800

    Yaaaaay, everthing is working!

--- JavaCode/moretesting/src/main/java/com/thisisatest/App.java
+++ JavaCode/moretesting/src/main/java/com/thisisatest/App.java
@@ -1,13 +1,20 @@
 package com.thisisatest;
-
-/**
- * Hello world!
- *
- */
+// This is a culmination of Udemy stuff, with methods focused on in particular
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World of maven!" );
+        calculateScore(true, 800, 5, 100);
+        calculateScore(true, 10_000, 8, 200);
+        
+    }
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalscore = score + (levelCompleted * bonus);
+            finalscore += 1000;
+            // System.out.println("Your final score was " + finalscore);
+            return finalscore;
+        }
+        return -1;
     }
 }
