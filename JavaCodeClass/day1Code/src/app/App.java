aa22b8ba707ebf118d14303d066256dfe6ffb0e8
admin user <adminuser@D-10-65-83-134.dhcp4.washington.edu>
2019-01-08 17:40:44 -0800

    class day 1

--- JavaCodeClass/day1Code/src/app/App.java
+++ JavaCodeClass/day1Code/src/app/App.java
@@ -1,7 +1,23 @@
 package app;
+/* This is a multiline comment 
+ * TCSS 142
+ */
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("This is a test for things and such");
+/**
+ * This program does some things and stuff
+ * 
+ * @author Kaleb Moreno (kalebm2@uw.edu)
+ * @version Winter 2018
+ */
+ public class App {
+     /**
+      * Run the overall program to print the....
+      * @param theArgs is the input from the keyboard
+      */
+    public static void main(String[] theArgs) {
+        // THis is where a single line comment should go that does stuff 
+        System.out.println("This is the first day of class");
+        System.out.println("This another print statement");
+
     }
 }
