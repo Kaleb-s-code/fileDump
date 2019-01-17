0f3477f8996989edb643337f378e93b4dfc936d3
admin user <adminuser@D-10-65-83-134.dhcp4.washington.edu>
2019-01-17 15:36:13 -0800

    game time

---
+++ JavaCodeClass/Day4Code & Lab/src/app/App.java
@@ -0,0 +1,8 @@
+package app;
+
+public class App {
+    public static void main(String[] theArgs) {
+        System.out.println("Hello Java");
+        System.out.println("THis is another test");
+    }
+}
