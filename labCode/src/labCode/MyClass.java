88203a08d78649b6c36b5c2ad07395ee387d998b
adminuser <adminuser@10.0.0.27>
2019-01-26 13:42:25 -0800

    some message from the MACCC

---
+++ labCode/src/labCode/MyClass.java
@@ -0,0 +1,10 @@
+package labCode;
+
+public class MyClass {
+    public void someClass() {
+	System.out.println("This is a test!!!!!");
+	System.out.println("This is a a diferent class which the driver is using");
+    }
+
+
+}
---
+++ labCode/src/labCode/testing.java
@@ -0,0 +1,11 @@
+package labCode;
+
+public class testing {
+    public static void main(String[] theArgs) {
+	System.out.println("This is a test");
+	
+	MyClass someObject = new MyClass();
+	
+	someObject.someClass();
+    }
+}
