7203b20426c1e19cbe78877bdad609bf69549112
TheBoss <kaleb.s.mor@gmail.com>
2019-01-26 14:33:06 -0800

    message from the pc

--- fromTheMac/src/fromTheMac/myClassFromMac.java
+++ fromTheMac/src/fromTheMac/myClassFromMac.java
@@ -5,6 +5,7 @@ public class myClassFromMac {
     public static void main(String[] args) {
 	System.out.println("THis is a test");
 	System.out.println("this is from the pc!");
+	System.out.println("This is another test from the pc");
     }
 
 }
--- myProject/src/myProject/AnewClass.java
+++ myProject/src/myProject/AnewClass.java
@@ -3,5 +3,6 @@ package myProject;
 public class AnewClass {
 	public void someMethod() {
 		System.out.println("THis is another message");
+		System.out.println("This is my final message that does things");
 	}
 }
