85910de97540596847211128b750ebc3094223fc
TheBoss <kaleb.s.mor@gmail.com>
2019-01-26 13:38:56 -0800

    a message

---
+++ myProject/src/myProject/AnewClass.java
@@ -0,0 +1,7 @@
+package myProject;
+
+public class AnewClass {
+	public void someMethod() {
+		System.out.println("THis is another message");
+	}
+}
--- myProject/src/myProject/someClassName.java
+++ myProject/src/myProject/someClassName.java
@@ -4,6 +4,7 @@ public class someClassName {
 
 	public static void main(String[] args) {
 		System.out.println("THis is a print statement");
+		System.out.println("This is another print statemnt!!");
 	}
 
 }
