0f237296906986fa68f5423d311811fa2fa56955
TheBoss <kaleb.s.mor@gmail.com>
2019-01-28 03:01:36 -0800

    moving stuff

--- ProgrammingProject3/src/myPackage/LostPuppy.java
+++ ProgrammingProject3/src/myPackage/LostPuppy.java
@@ -1,49 +1,82 @@
 package myPackage;
 import java.util.Arrays;
+import java.util.Random;
 
 public class LostPuppy {
-
+//	These are my fields
+	private char[][] myHiddingPlaces;
+	private int myFloorLocation;
+	private int myRoomLocation;
+	private char myWinner;
+	private boolean myFound;
+	
+//	This is my constructor 
 	public LostPuppy(int totalFloors, int totalRooms) {
-		
-		int[][] buildingDimensions;
-		buildingDimensions = new int[totalFloors][totalRooms];
-		System.out.println(Arrays.deepToString(buildingDimensions));
-		
+		this.myHiddingPlaces = new char[totalFloors][totalRooms];
+		System.out.println(Arrays.deepToString(this.myHiddingPlaces));
+		Random randomNumber = new Random();
+//		Arrays.fill(myHiddingPlaces, '@');
+//		This may not be the right number since its exclusive
+		this.myFloorLocation = randomNumber.nextInt(totalFloors);
+		this.myRoomLocation = randomNumber.nextInt(totalRooms);
+		this.myHiddingPlaces[myFloorLocation][myRoomLocation] = 'P';
+		this.myFound = false;
 	}
-
+//	Methods
 	public boolean roomSearchedAlready(int floor, int room) {
-		return false;
+		
+//		I need to find a way to store the guesses so that they aren't repeated..
+		
+		boolean returnValue = false;
+		
+		returnValue = true;
+		}
+
+		
+//	This method should return true if the room has already been searched
+
+		return returnValue;
 	}
 	
 	public boolean puppyLocation(int floor, int room) {
-		System.out.println("This is a print statemt");
-		return true;
+//	returns true if this is where the puppy was lost should NOT change any fields
+		boolean puppyFound = false;
+		if (floor == this.myFloorLocation) {
+			if (room == this.myRoomLocation) {
+				puppyFound = true;
+			}
+		}
+
+		return puppyFound;
 	}
 	
 	public boolean indicesOK(int floor, int room) {
-		System.out.println("THis is indiciesOK");
-		return true;
+//	This returns true if the floor and the room values are within the array indices range
+		boolean dimensionsOK = false;
+		if (floor <= this.myHiddingPlaces.length && room <= this.myHiddingPlaces[0].length) {
+			dimensionsOK = true;
+		}
+		return dimensionsOK;
 	}
 	
 	public int numberOfFloors() {
-		return 6;
+		return this.myHiddingPlaces.length;
 	}
 	
 	public int numberOfRooms() {
-		return 0;
+		return  this.myHiddingPlaces[0].length;
 	}
 	
-	public boolean searchRoom(int floor, int room, char player) {
-/*	This is supposed to set myHidingPlaces array at the received
-		and room location to the received player value 1, or 2.
-		Or when found, sets myWinner field to current player, and myFount to true
-*/
-		return true;
+	public boolean searchRoom(int floor, int room, char currentPlayer) {
+/*	returns true if the puppy is found. If not found sets myHidingPlaces array at the 
+		Received floor and room location to the received player value to a '1' or '2'. if found
+		sets myWinner field to current player AND sets myFound to true.
+*/		return false;
+	}
+	
+	public void toString(char[][] hidingPlaces) {
+//	This is the heavylifing of the program..see instructions
 	}
 
-	public String toString() {
-		
-		return ("THis is going to do the heavy lifting for the prints");
-	}
-
+	
 }
