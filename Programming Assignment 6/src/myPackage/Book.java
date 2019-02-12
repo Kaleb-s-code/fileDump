a0425c21266b29db31c089b83475eb4551e1e3aa
TheBoss <kaleb.s.mor@gmail.com>
2019-02-11 22:44:06 -0800

    Getting started

---
+++ Programming Assignment 6/src/myPackage/Book.java
@@ -0,0 +1,12 @@
+/*
+ * Multiline comment at the top of the document
+ */
+package myPackage;
+
+/**
+ * @author Kaleb Moreno (kalebm2@uw.edu)
+ *	@version 2/14/2019
+ */
+public class Book {
+
+}
---
+++ Programming Assignment 6/src/myPackage/LibraryDriver.java
@@ -0,0 +1,20 @@
+/*
+ * Multiline Comment
+ */
+package myPackage;
+
+/**
+ * @author Kaleb Moreno (kalebm2@uw.edu)
+ *	@version 2/14/2019
+ */
+public class LibraryDriver {
+
+	/**
+	 * @param theArgs
+	 */
+	public static void main(String[] theArgs) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
