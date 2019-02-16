f5682859f1ac47825d1bf29a1a9593692f67c311
adminuser <adminuser@admins-macbook-pro.local>
2019-02-16 12:13:10 -0800

    from the mac

--- Programming Assignment 6/src/myPackage/Book.java
+++ Programming Assignment 6/src/myPackage/Book.java
@@ -6,32 +6,31 @@ package myPackage;
 import java.util.ArrayList;
 
 /**
- * This class does things and stuff that a class should do 
- * And there are more things that it can do too.
+ * This class does things and stuff that a class should do And there are
+ * more things that it can do too.
  * 
  * @author Kaleb Moreno (kalebm2@uw.edu)
  * @version 2/14/2019
  */
 public class Book implements Comparable<Book> {
-	
+
 	/**
 	 * Private field
 	 */
 	private String myBookTitle;
-	
+
 	/**
 	 * Private field
 	 */
 	private ArrayList<String> myAuthorsNames;
-	
+
 	/**
 	 * This is the constructor that does some things
 	 * 
-	 * @param theTitle : The title of the books
+	 * @param theTitle   : The title of the books
 	 * @param theAuthors : The name of the author(s)
 	 */
-	public Book(final String theTitle, 
-													final ArrayList<String> theAuthors) {
+	public Book(final String theTitle, final ArrayList<String> theAuthors) {
 		if (theTitle == null || theAuthors == null) {
 			throw new IllegalArgumentException();
 		} else {
@@ -43,7 +42,8 @@ public class Book implements Comparable<Book> {
 
 	/**
 	 * Getter for the field
-	 * @return : the myBookTitle
+	 * 
+	 * @return : myBookTitle
 	 */
 	public String getTitle() {
 		return myBookTitle;
@@ -51,41 +51,47 @@ public class Book implements Comparable<Book> {
 
 	/**
 	 * Getter for the field
-	 * @return : the myAuthorsNames
+	 * 
+	 * @return : myAuthorsNames
 	 */
 	public ArrayList<String> getAuthors() {
 		return myAuthorsNames;
 	}
 
-	/* 
-	 * The toString() override
+	/**
+	 * This is a method that does things
+	 * 
+	 * @param theOther :
+	 * @return : boolean
 	 */
 	@Override
-	public String toString() {
-		//TODO
-		return "Book [myBookTitle=" + myBookTitle + 
-				", myAuthorsNames=" + myAuthorsNames + "]";
+	public boolean equals(final Object theOther) {
+		boolean result = false;
+		if (myBookTitle == theOther) {
+			result = true;
+		}
+		return true;
 	}
 	
 	/**
 	 * This is another method that does things
 	 * 
-	 * @param theOther : 
-	 * @return : 
+	 * @param theOther :
+	 * @return : integer
 	 */
+	@Override
 	public int compareTo(final Book theOther) {
-		//TODO
+		// TODO
 		return 0;
 	}
-	
-	/**
-	 * This is a method that does things
-	 * 
-	 * @param theOther : 
-	 * @return : 
+
+	/*
+	 * The toString() override
 	 */
-	public boolean equals(final Object theOther) {
-		//TODO
-		return true;
+	@Override
+	public String toString() {
+		// TODO
+		return "Book [myBookTitle=" + myBookTitle + ", myAuthorsNames=" + 
+				myAuthorsNames + "]";
 	}
 }
